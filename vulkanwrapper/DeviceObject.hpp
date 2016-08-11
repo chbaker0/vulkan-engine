1994085d802133fb4dda955acb27a5a9e19dba4d
Collin Baker <chbaker0@gmail.com>
2016-08-10 21:17:11 -0700

    General device object wrapper

---
+++ vulkanwrapper/DeviceObject.hpp
@@ -0,0 +1,87 @@
+/**
+ * \brief Generic wrapper around Vulkan objects owned by VkDevice
+ */
+
+#ifndef VULKANWRAPPER_DEVICEOBJECT_HPP
+#define VULKANWRAPPER_DEVICEOBJECT_HPP
+
+#include <utility>
+
+#define VK_NO_PROTOTYPES
+#include <vulkan/vulkan.h>
+
+namespace vkw
+{
+namespace detail
+{
+
+template <typename HandleType>
+class DeviceObject
+{
+public:
+    typedef void (VKAPI_PTR *Destroyer)(VkDevice, HandleType, const VkAllocationCallbacks *);
+
+    DeviceObject(VkDevice deviceHandle_in, HandleType handle_in, Destroyer destroyer_in) noexcept
+        : deviceHandle(deviceHandle_in)
+        , handle(handle_in)
+        , destroyer(destroyer_in)
+    {
+    }
+
+    ~DeviceObject()
+    {
+        if (deviceHandle != nullptr && handle_in != nullptr && destroyer != nullptr)
+        {
+            destroyer(deviceHandle, handle, nullptr);
+        }
+    }
+
+    DeviceObject(const Device& other) = delete;
+    DeviceObject(Device&& other) noexcept
+        : Device(nullptr, nullptr, nullptr)
+    {
+        std::swap(deviceHandle, other.deviceHandle);
+        std::swap(handle, other.handle);
+        std::swap(destroyer, other.destroyer);
+    }
+
+    DeviceObject& operator = (const DeviceObject& other) = delete;
+    DeviceObject& operator = (DeviceObject&& other)
+    {
+        std::swap(deviceHandle, other.deviceHandle);
+        std::swap(handle, other.handle);
+        std::swap(destroyer, other.destroyer);
+        return *this;
+    }
+
+    HandleType operator * () const noexcept
+    {
+        return handle;
+    }
+
+    const HandleType& getHandle() const noexcept
+    {
+        return handle;
+    }
+
+    HandleType releaseHandle() noexcept
+    {
+        HandleType temp = handle;
+
+        deviceHandle = nullptr;
+        handle = nullptr;
+        destroyer = nullptr;
+
+        return temp;
+    }
+
+private:
+    VkDevice deviceHandle;
+    HandleType handle;
+    Destroyer destroyer;
+};
+
+} // namespace detail
+} // namespace vkw
+
+#endif // VULKANWRAPPER_DEVICEOBJECT_HPP
